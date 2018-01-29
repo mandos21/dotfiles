a0813cd041ff37a699d762a36ed9eb85c8ff1645
Matthew DeGenaro <mt.degenaro@gmail.com>
2018-01-28 20:00:16 -0500

    added dynamic window size changing to i3

--- install.sh
+++ install.sh
@@ -16,3 +16,6 @@ ln -s ~/.config/dotfiles/ncmpcpp/.ncmpcpp ~/.ncmpcpp/config
 ln -s ~/.config/dotfiles/x/.xbindkeys ~/.xbindkeys
 ln -s ~/.config/dotfiles/x/.xinitrc ~/.xinitrc
 ln -s ~/.config/dotfiles/x/.Xresources ~/.Xresources
+
+ln -s ~/.config/dotfiles/polybar/config ~/.config/polybar/config
+ln -s ~/.config/dotfiles/polybar/launch.sh ~/.config/polybar/launch.sh
